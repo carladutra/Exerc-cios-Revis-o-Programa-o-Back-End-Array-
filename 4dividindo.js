1fabe240c11b551785145e65ecc87cb66bf3649a
Carla Dutra <100164378+carladutra@users.noreply.github.com>
2024-04-28 19:45:20 -0300

    Add files via upload

---
+++ 4dividindo.js
@@ -0,0 +1,9 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
+
+//Vamos selecionar os 10 primeiros alunos para colocá-los na sala 1, usando o método slice(). Em inglês, o termo slice significa "fatiar" ou "dividir":
+
+//slice
+// alunos.slice(onde iniciar a divisão, onde termina) 
+
+
+//usando o alunos.length
---
+++ 5splice.js
@@ -0,0 +1,9 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+// Ana e Caio mudaram de escola e o Rodrigo entrou nessa sala. Nosso desafio será atualizar a lista de chamada, removendo os nomes de Ana e Caio, e inserindo Rodrigo.
+
+//nomes.splice(indice do elemento que queremos remover, quantidade de elementos que quero remover)
+
+//push ou splice
+
+//nomes.splice(indice do elemento que queremos remover, quantidade de elementos que quero remover, se quiser adicionar algo)
